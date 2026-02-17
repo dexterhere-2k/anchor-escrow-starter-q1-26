d78e2b60c699f0d686093dba4c38a962558f3c11
dexterhere-2k <deepakmirchandani.ai28@jecrc.ac.in>
2026-02-17 22:58:00 +0530

    done

---
+++ programs/anchor-escrow-starter-q1-26/src/instructions/make.rs
@@ -0,0 +1,71 @@
+use crate::Escrow;
+use anchor_lang::prelude::*;
+use anchor_spl::{associated_token::AssociatedToken, token::{transfer_checked, TransferChecked}, token_interface::{ Mint, TokenAccount, TokenInterface}};
+#[derive(Accounts)]
+#[instruction(seeds:u64)]
+pub struct Make<'info> {
+    #[account(mut)]
+    pub maker: Signer<'info>,
+    #[account(mint::token_program = token_program)]
+    pub mint_a: InterfaceAccount<'info, Mint>,
+    #[account(mint::token_program = token_program)]
+    pub mint_b: InterfaceAccount<'info, Mint>,
+    pub system_program: Program<'info, System>,
+    #[account(
+        mut,
+        associated_token::mint=mint_a,
+        associated_token::authority=maker,
+        associated_token::token_program=token_program
+    )]
+    pub maker_ata_a: InterfaceAccount<'info, TokenAccount>,
+    #[account(
+        init,
+        payer=maker,
+        seeds=[b"escrow",maker.key().as_ref(),seeds.to_le_bytes().as_ref()],
+        space=8+Escrow::INIT_SPACE,
+        bump
+    )]
+    pub escrow: Account<'info, Escrow>,
+    #[account(
+        init,
+        payer=maker,
+        associated_token::mint=mint_a,
+        associated_token::authority=escrow,
+        associated_token::token_program=token_program
+
+    )]
+    pub vault: InterfaceAccount<'info, TokenAccount>,
+    pub token_program: Interface<'info, TokenInterface>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+}
+
+
+
+impl<'info> Make<'info>{
+    pub fn init_escrow(&mut self,seed:u64,receive:u64,bump:&MakeBumps)->Result<()>{
+        self.escrow.set_inner(Escrow {
+            seed,
+            maker:*self.maker.key,
+            mint_a:self.mint_a.key(),
+            mint_b:self.mint_b.key(),
+            receive,
+            bump:bump.escrow,
+
+        });
+        
+        
+        
+        Ok(())
+    }
+    pub fn deposit(&mut self,deposit:u64)->Result<()>{
+        let transfer_accounts=TransferChecked{
+            from:self.maker_ata_a.to_account_info(), 
+            mint:self.mint_a.to_account_info(),
+            to:self.vault.to_account_info(),
+            authority:self.maker.to_account_info(),
+        };
+        let cpi_ctx=CpiContext::new(self.token_program.to_account_info(),transfer_accounts);
+        transfer_checked(cpi_ctx, deposit,self.mint_a.decimals)
+        // Ok(())
+    }
+}
--- programs/anchor-escrow-starter-q1-26/src/lib.rs
+++ programs/anchor-escrow-starter-q1-26/src/lib.rs
@@ -1,14 +1,27 @@
 use anchor_lang::prelude::*;
-
+pub mod state;
+pub use state::*;
+pub mod instructions;
+pub use instructions::*;
 declare_id!("93UWPgTx9eMd8rASGvEsfXcAdphAuSnPcSzTpasdruUm");
 
 #[program]
 pub mod anchor_escrow_starter_q1_26 {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn make(ctx: Context<Make>, seeds: u64, deposit: u64, receive: u64) -> Result<()> {
         msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
+        ctx.accounts.init_escrow(seeds, receive, &ctx.bumps)?;
+        ctx.accounts.deposit(deposit)
+        // Ok(())
+    }
+    pub fn refund(ctx: Context<Refund>) -> Result<()> {
+        ctx.accounts.refund_and_close_vault()
+    }
+    pub fn take(ctx: Context<Take>) -> Result<()> {
+        ctx.accounts.maker_transfer()?;
+        ctx.accounts.vault_transfer()?;
+        ctx.accounts.close_vault()
     }
 }
 
